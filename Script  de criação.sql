9655869c841e5ee2e57b19bd13fa7d0bf52cf453
Ewerthon Santana <87082565+EwerthonSantana@users.noreply.github.com>
2021-10-22 23:55:29 -0300

    Add files via upload

---
+++ Script  de criação.sql
@@ -0,0 +1,68 @@
+create database EstrelaDaMorte
+
+USE EstrelaDaMorte
+
+--************************************************  PLANETAS *******************************************************************************
+CREATE TABLE Planetas(
+	IdPlaneta int NOT NULL primary key,
+	Nome varchar(50) NOT NULL,
+	Rotacao float NOT NULL,
+	Orbita float NOT NULL,
+	Diametro float NOT NULL,
+	Clima varchar(50) NOT NULL,
+	Populacao int NOT NULL,
+)
+GO
+--**************************************************** NAVES *******************************************************************************
+CREATE TABLE Naves(
+	IdNave int NOT NULL primary key,
+	Nome varchar(100) NOT NULL,
+	Modelo varchar(150) NOT NULL,
+	Passageiros int NOT NULL,
+	Carga float NOT NULL,
+	Classe varchar(100) NOT NULL,
+)
+GO
+--**************************************************** PILOTOS ********************************************************************************
+CREATE TABLE Pilotos(
+	IdPiloto int NOT NULL primary key,
+	Nome varchar(200) NOT NULL,
+	AnoNascimento varchar(10) NOT NULL,
+	IdPlaneta int NOT NULL,
+)
+GO
+ALTER TABLE Pilotos  ADD  CONSTRAINT FK_Pilotos_Planetas FOREIGN KEY(IdPlaneta)
+REFERENCES Planetas (IdPlaneta)
+GO
+ALTER TABLE Pilotos CHECK CONSTRAINT FK_Pilotos_Planetas
+GO
+--************************************************** PILOTOS NAVES *********************************************************************************
+CREATE TABLE PilotosNaves(
+	IdPiloto int NOT NULL,
+	IdNave int NOT NULL,
+	FlagAutorizado bit NOT NULL,
+)
+GO
+ALTER TABLE PilotosNaves ADD CONSTRAINT PK_PilotosNaves PRIMARY KEY (IdPiloto, IdNave);
+GO
+ALTER TABLE PilotosNaves  ADD CONSTRAINT FK_PilotosNaves_Pilotos FOREIGN KEY(IdPiloto)
+REFERENCES Pilotos (IdPiloto)
+GO
+ALTER TABLE PilotosNaves  ADD CONSTRAINT FK_PilotosNaves_Naves FOREIGN KEY(IdNave)
+REFERENCES Naves (IdNave)
+GO
+ALTER TABLE PilotosNaves  ADD CONSTRAINT DF_PilotosNaves_FlagAutorizado  DEFAULT (1) FOR FlagAutorizado
+GO
+--************************************************** HISTÓRICO DE VIAGENS ********************************************************************************
+CREATE TABLE HistoricoViagens(
+	IdNave int NOT NULL,
+	IdPiloto int NOT NULL,
+	DtSaida datetime NOT NULL,
+	DtChegada datetime NULL
+)
+GO
+ALTER TABLE HistoricoViagens  ADD  CONSTRAINT FK_HistoricoViagens_PilotosNaves FOREIGN KEY(IdPiloto, IdNave)
+REFERENCES PilotosNaves (IdPiloto, IdNave)
+GO
+ALTER TABLE HistoricoViagens CHECK CONSTRAINT FK_HistoricoViagens_PilotosNaves
+GO
